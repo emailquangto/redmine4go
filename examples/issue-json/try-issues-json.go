88af62c529100de50dce39747890bfd43b7e3429
emailquangto <emailquangto@gmail.com>
2021-10-02 13:46:18 +0700

    refactor GetIssues() mapping original api GET/issues

--- examples/issue-json/try-issues-json.go
+++ examples/issue-json/try-issues-json.go
@@ -21,10 +21,10 @@ func main() {
 
 	// default parameters of querying issues
 	paras := &redmine4go.IssueListParameter{
-		0,  // Offset
-		25, // Limit
-		"", // Sort
-		"", // Include
+		nil, // Offset = 0
+		nil, // Limit = 25
+		"",  // Sort
+		"",  // Include
 	}
 
 	// default filters of querying issues
@@ -38,30 +38,47 @@ func main() {
 		nil, // ParentId
 	}
 
-	issueList, error := c.GetIssueListOfProject(projectId, paras, filters)
+	// get list of open issues that user can access
+	issueList, error := c.GetIssues(paras, filters)
 	if error == nil {
 		fmt.Printf("Number of issues = %d\n", issueList.TotalCount)
-		fmt.Printf("issue 1 - Project = %s\n", issueList.Issues[0].Project.Name)
-		fmt.Printf("issue 1 - ID = %d\n", issueList.Issues[0].ID)
-		fmt.Printf("issue 1 - Subject = %s\n", issueList.Issues[0].Subject)
-		fmt.Printf("issue 1 - Status = %s\n", issueList.Issues[0].Status.Name)
-		fmt.Printf("issue 1 - Author = %s\n", issueList.Issues[0].Author.Name)
-		fmt.Printf("issue 1 - Assigned To = %s\n", issueList.Issues[0].AssignedTo.Name)
+		if issueList.TotalCount > 0 {
+			fmt.Printf("issue 1 - Project = %s\n", issueList.Issues[0].Project.Name)
+			fmt.Printf("issue 1 - ID = %d\n", issueList.Issues[0].ID)
+			fmt.Printf("issue 1 - Subject = %s\n", issueList.Issues[0].Subject)
+			fmt.Printf("issue 1 - Status = %s\n", issueList.Issues[0].Status.Name)
+			fmt.Printf("issue 1 - Author = %s\n", issueList.Issues[0].Author.Name)
+			fmt.Printf("issue 1 - Assigned To = %s\n", issueList.Issues[0].AssignedTo.Name)
+		}
 	} else {
 		fmt.Printf("%s\n", error)
 	}
 
 	fmt.Printf("%s\n", "=====*****=====")
 
-	issues, error := c.GetIssuesOfProject(projectId, paras, nil)
+	// default filters of querying issues
+	filters = &redmine4go.IssueListFilter{
+		nil,       // IssueId
+		projectId, // ProjectId
+		nil,       // SubprojectId
+		nil,       // TrackerId
+		nil,       // StatusId
+		nil,       // AssignedToId
+		nil,       // ParentId
+	}
+
+	// get list of open issues of a project that user can access
+	issueList, error = c.GetIssues(paras, filters)
 	if error == nil {
-		fmt.Printf("Number of issues = %d\n", len(issues))
-		fmt.Printf("issue 1 - Project = %s\n", issues[0].Project.Name)
-		fmt.Printf("issue 1 - ID = %d\n", issues[0].ID)
-		fmt.Printf("issue 1 - Subject = %s\n", issues[0].Subject)
-		fmt.Printf("issue 1 - Status = %s\n", issues[0].Status.Name)
-		fmt.Printf("issue 1 - Author = %s\n", issues[0].Author.Name)
-		fmt.Printf("issue 1 - Assigned To = %s\n", issues[0].AssignedTo.Name)
+		fmt.Printf("Number of issues = %d\n", issueList.TotalCount)
+		if issueList.TotalCount > 0 {
+			fmt.Printf("issue 1 - Project = %s\n", issueList.Issues[0].Project.Name)
+			fmt.Printf("issue 1 - ID = %d\n", issueList.Issues[0].ID)
+			fmt.Printf("issue 1 - Subject = %s\n", issueList.Issues[0].Subject)
+			fmt.Printf("issue 1 - Status = %s\n", issueList.Issues[0].Status.Name)
+			fmt.Printf("issue 1 - Author = %s\n", issueList.Issues[0].Author.Name)
+			fmt.Printf("issue 1 - Assigned To = %s\n", issueList.Issues[0].AssignedTo.Name)
+		}
 	} else {
 		fmt.Printf("%s\n", error)
 	}
